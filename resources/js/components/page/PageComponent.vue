beacc4a8de8e734fd6890d987586c2bb9b26ebf9
Сергей <1@php-cat.com>
2022-08-13 05:30:29 +0500

    77

--- resources/js/components/page/PageComponent.vue
+++ resources/js/components/page/PageComponent.vue
@@ -49,7 +49,7 @@
             ></iframe>
           </div>
         </div>
-        <div class="row" v-else>
+        <div class="row links" v-else>
           <div class="col-12">
             <div v-html="pageData.html"></div>
           </div>
@@ -90,7 +90,7 @@ const stopWatch = watchEffect(() => {
 </script>
 
 <style scoped>
-a {
+.links a {
   color: blue;
   text-decoration: underline;
 }
