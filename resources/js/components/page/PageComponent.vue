b0bfd26ef39cdbfeccdc768f769ce806d15e262b
Сергей <1@php-cat.com>
2022-08-13 05:24:09 +0500

    11

--- resources/js/components/page/PageComponent.vue
+++ resources/js/components/page/PageComponent.vue
@@ -10,8 +10,12 @@
       <div class="row" v-if="pageLoading">
         <div class="col-12 text-center">
           .. загружаю данные ..
-          <br/>
-        <img src="/storage/site/img/loader.gif" alt="" style="width:120px;" />
+          <br />
+          <img
+            src="/storage/site/img/loader.gif"
+            alt=""
+            style="width: 120px;"
+          />
         </div>
       </div>
       <div class="row" v-else-if="pageError">
@@ -29,8 +33,7 @@
       </div>
       <template v-else>
         <div class="row" v-if="$route.params.id == 'contact'">
-
-        <!-- <div class="col-12"><page_contact></page_contact></div> -->
+          <!-- <div class="col-12"><page_contact></page_contact></div> -->
 
           <div class="col-6">
             <div v-html="pageData.html"></div>
@@ -38,10 +41,15 @@
           <div class="col-6">
             <!-- map -->
             <!-- <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Aa1c21a22720667468e2e00b1009b667e2403d7dfe330b5d32d3f3a4ab255bad4&amp;width=100%25&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script> -->
-            <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Aa1c21a22720667468e2e00b1009b667e2403d7dfe330b5d32d3f3a4ab255bad4&amp;source=constructor" width="100%" height="600" frameborder="0"></iframe>
+            <iframe
+              src="https://yandex.ru/map-widget/v1/?um=constructor%3Aa1c21a22720667468e2e00b1009b667e2403d7dfe330b5d32d3f3a4ab255bad4&amp;source=constructor"
+              width="100%"
+              height="600"
+              frameborder="0"
+            ></iframe>
           </div>
         </div>
-        <div class="row page-links" v-else >
+        <div class="row page-links" v-else>
           <div class="col-12">
             <div v-html="pageData.html"></div>
           </div>
@@ -56,7 +64,6 @@
 </template>
 
 <script setup>
-
 import { ref, watchEffect } from 'vue'
 import page from './../../use/page.ts'
 
@@ -83,4 +90,8 @@ const stopWatch = watchEffect(() => {
 </script>
 
 <style scoped>
-.page-links a{ color: blue; }</style>
+.page-links a {
+  color: blue;
+  text-decoration: underline;
+}
+</style>
