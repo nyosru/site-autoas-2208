54737809ffcde216fd3d8a69708708eb17cf9e28
Сергей <1@php-cat.com>
2022-08-14 08:30:20 +0500

    111

--- app/Http/Controllers/GoodController.php
+++ app/Http/Controllers/GoodController.php
@@ -33,6 +33,8 @@ class GoodController extends Controller
         $s = explode(' ', $request->search);
         // $s = explode(' ', $request->search);
 
+        
+
         return new GoodCollection(Good::with('analog')->where(function ($query) use ($s, $request) {
             foreach ($s as $v) {
 
