6e6a06a59bf89df8f4328d903d7c92a9f45acdc9
Sergey <1@php-cat.com>
2024-01-22 19:03:33 +0500

    57

---
+++ database/migrations/2024_01_16_042547_add_canceled_status_to_orders_table.php
@@ -0,0 +1,75 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class AddCanceledStatusToOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        DB::statement("
+            ALTER TABLE `orders` CHANGE `status` `status`
+            ENUM(
+            'new',
+            'job',
+            'sended',
+            'posted',
+            'finish',
+            'canceled'
+            ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'new' COMMENT '
+        new = Новые \r\n
+        job = В работе \r\n
+        sended = Отправлен\r\n
+        posted = Доставлен\r\n
+        finish = Завершён\r\n
+        canceled = Отменён\r\n
+        ';
+        ");
+
+//        'all' => 'Все',
+//        'new' => 'Новые',
+//        'job' => 'В работе',
+//        'sended' => 'Отправлен',
+//        'posted' => 'Доставлен',
+//        'finish' => 'Завершён',
+//        'canceled' => 'Отменён',
+
+//        Schema::table('orders', function (Blueprint $table) {
+//            // Для отмены изменений, если это необходимо
+//            $table->dropColumn('status');
+//        });
+//        Schema::table('orders', function (Blueprint $table) {
+//            $table->enum('status', [
+//                'new',
+//                'start',
+//                'maked',
+//                'sended',
+//                'finish',
+//                'canceled'  // Добавляем новое значение 'canceled'
+//            ])->default('new')->comment('new новый /
+//            start принят в работу, собираем /
+//            maked собран /
+//            sended отправлен /
+//            finish заказ получен, выполнен, закрыт /
+//            canceled отменён'); // Обновите комментарий, если необходимо
+//        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+}
