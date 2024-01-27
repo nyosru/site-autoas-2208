f252fca1a85c453a740c2ac96dafd0aeb79e1144
Sergey <1@php-cat.com>
2024-01-27 22:17:27 +0500

    64

--- routes/api.php
+++ routes/api.php
@@ -53,7 +53,8 @@ Route::get('/getTest2', function (Request $request) {
 
 //Route::get('allautoparts/{search}', function (Request $request) {
 Route::get('allautoparts/{search}', function ( $search ) {
-    return $search;
+    return file_get_contents('https://api74.php-cat.com/a/api.php?ss=da&search='.$search );
+//    return $search;
 });
 
 Route::get('/getTest', function (Request $request) {
