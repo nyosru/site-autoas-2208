014388792d5245a85289053b91f688638a60e022
nyosru <nyos@rambler.ru>
2026-05-28 00:25:02 +0500

    ++

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,11 +7,12 @@
 
     <title>Детали Авто (Магазин автозапчастей в Тюмени!)</title>
 
-    <link href="{{ asset('css/app.css') }}?s=2208120328" rel="stylesheet" />
-    <link rel="stylesheet" href="{{ asset('storage/css/ionicons.min.css') }}" />
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet"/>
+    <link href="{{ asset('css/app.css') }}?s=2208120328" rel="stylesheet"/>
+    <link rel="stylesheet" href="{{ asset('css/ionicons.min.css') }}"/>
 
-    <link rel="stylesheet" href="{{ asset('storage/css/style.css') }}?s=2208120328" />
-    <link rel="icon" type="image/png" href="/storage/site/img/logo47.png" />
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}?s=2208120328"/>
+    <link rel="icon" type="image/png" href="/storage/site/img/logo47.png"/>
 
     <meta property="og:title" content="Детали Авто запчасти">
     <meta property="og:site_name" content="avto-as.ru">
@@ -33,31 +34,31 @@
 {{--<br/>--}}
 {{--<br/>--}}
 
-    <div id="app"></div>
+<div id="app"></div>
 
-    <!-- Yandex.Metrika counter -->
-    <script type="text/javascript">
-        (function(m, e, t, r, i, k, a) {
-            m[i] = m[i] || function() {
-                (m[i].a = m[i].a || []).push(arguments)
-            };
-            m[i].l = 1 * new Date();
-            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(
-                k, a)
-        })
-        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function (m, e, t, r, i, k, a) {
+        m[i] = m[i] || function () {
+            (m[i].a = m[i].a || []).push(arguments)
+        };
+        m[i].l = 1 * new Date();
+        k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(
+            k, a)
+    })
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-        ym(89950536, "init", {
-            clickmap: true,
-            trackLinks: true,
-            accurateTrackBounce: true
-        });
-    </script>
-    <noscript>
-        <div><img src="https://mc.yandex.ru/watch/89950536" style="position:absolute; left:-9999px;" alt="" />
-        </div>
-    </noscript>
-    <!-- /Yandex.Metrika counter -->
+    ym(89950536, "init", {
+        clickmap: true,
+        trackLinks: true,
+        accurateTrackBounce: true
+    });
+</script>
+<noscript>
+    <div><img src="https://mc.yandex.ru/watch/89950536" style="position:absolute; left:-9999px;" alt=""/>
+    </div>
+</noscript>
+<!-- /Yandex.Metrika counter -->
 
 </body>
 
